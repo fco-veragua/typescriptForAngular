cfb6d401744bd4022ac8da6f0948184d58a5ba1d
fco-veragua <franciscoveraguacabrera@gmail.com>
2022-05-09 14:46:11 +0200

    More tests with the OR operator

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@
 
 let nombre: string = 'Fco' // si no se especifica, ts infiere el tipo (el compilador determina el tipo de la variable)
 
-let hp: number | string = 123 // con el operador '|' (OR) indico que la variable acepta valores de 2 tipos
+let hp: number | string | number[] = 123 // con el operador '|' (OR) indico que la variable acepta valores de 2 tipos
 
-hp = 'FULL' // continuando con la aclaración anterior, puede ser que necesite almacenar ahora una cadena
+hp = [1, 2] // continuando con la aclaración anterior, puede ser que necesite almacenar ahora una cadena
 
 console.log(nombre, hp)
