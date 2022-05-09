a6b56efa5ea0ec75e4ef843e02f55613c3c7599b
fco-veragua <franciscoveraguacabrera@gmail.com>
2022-05-09 14:42:59 +0200

    OR operator to indicate the data types that the variable accepts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-
-
-console.log('Hola Mundo!!!!!!!!!!!!!');
-
 /*
-    ===== Código de TypeScript =====
+    ==== Typescript code ====
 */
 
+let nombre: string = 'Fco' // si no se especifica, ts infiere el tipo (el compilador determina el tipo de la variable)
+
+let hp: number | string = 123 // con el operador '|' (OR) indico que la variable acepta valores de 2 tipos
+
+hp = 'FULL' // continuando con la aclaración anterior, puede ser que necesite almacenar ahora una cadena
+
+console.log(nombre, hp)
